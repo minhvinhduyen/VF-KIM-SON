af30c332f4568bbb0424644e53cd9d06873e2c96
minhvinhduyen <minhvinhduyen@users.noreply.github.com>
2026-08-20 17:23:02 +0700

    Fix CORS via Netlify rewrites/proxy to cPanel

--- cpanel-deploy/app.js
+++ cpanel-deploy/app.js
@@ -8,7 +8,13 @@ const fs = require('fs');
 const mysql = require('mysql2/promise');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'x-facility-id'],
+    credentials: true
+}));
+app.options('*', cors());
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
